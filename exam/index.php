f73a6505be6de246bfcf8d28680fe5438a5d14f4
alexis46731 <alexlegamer17dragoon@gmail.com>
2025-01-17 11:43:00 +0100

    Update index.php

--- exam/index.php
+++ exam/index.php
@@ -1,23 +1,3 @@
-<?php
-// On démarre une session
-session_start();
-
-// On inclut la connexion à la base
-require_once('connect.php');
-
-$sql = 'SELECT * FROM `liste`';
-
-// On prépare la requête
-$query = $db->prepare($sql);
-
-// On exécute la requête
-$query->execute();
-
-// On stocke le résultat dans un tableau associatif
-$result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-require_once('close.php');
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -26,25 +6,6 @@ require_once('close.php');
     <title>Liste des produits</title>
     <link rel="stylesheet" href="./style/style.css">
 <body>
-    <main class="container">
-        <div class="row">
-            <section class="col-12">
-            <?php
-                    if(!empty($_SESSION['erreur'])){
-                        echo '<div class="alert alert-danger" role="alert">
-                                '. $_SESSION['erreur'].'
-                            </div>';
-                        $_SESSION['erreur'] = "";
-                    }
-                ?>
-                <?php
-                    if(!empty($_SESSION['message'])){
-                        echo '<div class="alert alert-success" role="alert">
-                                '. $_SESSION['message'].'
-                            </div>';
-                        $_SESSION['message'] = "";
-                    }
-                ?>
                 <div class="contact-form-container">
                 <h2>Formulaire de Contact</h2>
                 <form action="add.php" method="$_POST">
